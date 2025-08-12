d65238e310886910576d1fbec82bf6acbfba15b8
Benjamin Piatkowski <58381976+ben4235@users.noreply.github.com>
2025-08-12 10:30:03 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,25 @@
 
-const CACHE = 'focus-forge-v1';
+const CACHE = 'focus-forge-v2';
 const ASSETS = [
-  './','./index.html','./style.css','./game.js','./manifest.json',
-  './icons/icon-192.png','./icons/icon-512.png'
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))));
   self.clients.claim();
 });
-self.addEventListener('fetch', e=>{
+self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
-  if(url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
   }
 });
